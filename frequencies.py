26bd01619cb109876a5afbb5205246030311727f
Johnny-32 <janglowacki32@gmail.com>
2026-07-19 11:42:20 +0200

    Removed documentation from this file and moved it into frequencies.md

--- frequencies.py
+++ frequencies.py
@@ -1,85 +1,3 @@
-# Example 1. (Made in July 2026 - Holiday timetable) ------------------------------------------------------------------------------------------------
-# Line: 32
-# Direction: Daszyńskiego
-# Bus stop: Plac Litewskiego 02 (Index - 1022)
-# Timetable:
-# 
-#     5   6    7    8   9   10  11   12  13   14  15   16   17   18   19   20   21   22   23 
-#    05  07   26   15  02   14  02   14  02   14  02   17   04   05   03   03   06   06   05
-#    37  34   51   38  26   38  26   38  26   38  29   41   35   33   33   36   36   36  NaN
-#   NaN  59  NaN  NaN  50  NaN  50  NaN  50  NaN  53  NaN  NaN  NaN  NaN  NaN  NaN  NaN  NaN
-# 
-# In this example we will be sticking to hours 5-9
-# 1. Calculating differences
-# 5:37 - 5:05 = 32
-# 6:07 - 5:37 = -30 + 60 = 30
-# 6:34 - 6:07 = 27
-# 6:59 - 6:34 = 25
-# 7:26 - 6:59 = -33 + 60 = 27
-# 7:51 - 7:26 = 25
-# 8:15 - 7:51 = -36 + 60 = 24
-# 8:38 - 8:15 = 23
-# 9:02 - 8:38 = 24
-# 9:26 - 9:02 = 24
-# 9:50 - 9:26 = 24
-# 
-# So using our common sense we can see that frequencies are:
-# 5:05 - 6:07 --- 30 minutes
-# 6:07 - 7:51 --- it makes sense to make it 24 minutes, but in reality it's 26 minutes
-# 7:51 - 9:50 --- 24 minutes
-# 
-# 
-# Now we can group the differences, let's say that the tolerance between differences is 2 minutes
-# |32 - 30| = 2 -> group 1.
-# |30 - 30| = 0 -> group 1.
-# |27 - 24| = 3 and |27 - 30| = 3 -> not matched
-# |25 - 24| = 1 -> group 2.
-# |27 - 24| = 3 and |27 - 30| = 3 -> not matched
-# |25 - 24| = 1 -> group 2.
-# |24 - 24| = 0 -> group 2
-# |23 - 24| = 1 -> group 2
-# |24 - 24| = 0 -> group 2
-# |24 - 24| = 0 -> group 2
-# 
-# Now we have two unmathed elements that have neighboring elements that are groups,
-# so we can add them to a group, which has a neighboring element that is closest to an unmatched element.
-
-# In both cases we add unmatched elements to the group 2. and finally we get:
-# [[32, 30], [27, 25, 27, 25, 24, 23, 24, 24]]
-# 
-# HARDER TO IMPLEMENT FREQUENCY ALGORITHM, BUT IT COULD WORK BETTER WITH UNCOMMON FREQUENCIES (35, 42 minutes etc.) 
-# COULD BE USED IN THE FUTURE
-# 
-# Now we can group the differences, let's say that the tolerance between differences is 2 minutes
-# 32 - 30 = 2 -> Start of group 1.
-# 30 - 27 = 3 -> End of group 1.
-# 27 - 25 = 2 -> Start of group 2.
-# 25 - 27 = 2 -> Both in group 2.
-# 27 - 25 = 2 -> Both in group 2
-# 25 - 24 = 1 -> Both in group 2
-# 24 - 23 = 1 -> Both in group 2
-# 23 - 24 = 1 -> Both in group 2
-# 24 - 24 = 0 -> Both in group 2
-# 24 - 24 = 0 -> Both in group 2
-# 
-# So now we have 2 groups and now we can calculate the mean from all values in each group and see which predefined frequency (for ex. 20, 24, 30, ...) will be the closest to the mean and assign that label
-# for ex. group 1. (32 + 30) / 2 = 31 -> which is close to 30, so we will asign a 30 minute frequency label to that group
-# group 2. after calculations will have 24 minute frequency label
-# 
-# We can see that the last difference for the group 1 is 30, and the first for the group 2. is 27,
-# so that means if we skip "End of group 1." we have all the differences in correct groups (obviously we need to delete duplicates)
-# 
-# Now we need to extract timestamps from key differences (30 and 27)
-# We can do this by - extracting the key from dictionary:
-# in group 1. we have a key difference of 30, which translates to 6:07 - 5:37
-# in group 2. we have a key difference of 27, which translates to 6:34 - 6:07
-# 
-# Beacuse the first number of the 1. group and the second number of the 2. group matches, that means that 6:07 will be a breakpoint for frequency labels
-# 
-# As a result we got frequencies which are the same as we got above using common sense
-# 
-# ??? Maybe add calculating frequencies by getting the timetable from the first stop of a route, beacuse the departures there are more consistent
-
 import timetable
 from math import isnan
 
@@ -163,18 +81,7 @@ for idx in range(len(timetable.df_list)):
     group_list_for_a_timetable_period_container.append(group_list_for_a_timetable_period)
     not_matched_index_list_container.append(not_matched_index_list)
 
-
-# Now we have a list of not matched elements and they can be sorted into three categories:
-# Cat 1. Not matched elements that are surrounded by groups (ex. [..., group0, notmatched0, group1, ...])
-# Cat 2. Not matched elements that have a neighboring element which is a group (either to the left or to the right) (ex. [..., group0, notmatched0, notmatched1, ...])
-# Cat 3. Elements that are surrounded by not matched list elements (ex. [..., notmatched0, notmatched1, notmatched2, ...])
-# 
-# So my soultion is to append every element to a group in following steps:
-# 1. If we find an element in Cat 1. we append it to the group that has the element that is the closest to an unmatched element
-# ex. [[..., 30], 27, [25, ...]] we append that element to the group to the right, beacuse |27 - 30| > |27 - 25|
-# 2. If we find an element in Cat 2. we append that element to the group that neighbors this element
-# 
-# By doing this two steps we should transform elements in Cat 3. to an element in either Cat 1. or Cat 2.
+    # Joining not matched elements into groups
 
     current_group_list = group_list_for_a_timetable_period_container[idx]
     current_not_matched_index_list = not_matched_index_list_container[idx]
@@ -191,7 +98,7 @@ for idx in range(len(timetable.df_list)):
 
             current_val = get_val(item)
 
-            # Finding a Cat 1. element - exactly one neighbor that is a group
+            # Finding an unmatched element with exactly one neighbor that is a group
             if left_is_group ^ right_is_group:
                 if left_is_group:
                     left_group = current_group_list[idx_gr - 1]
@@ -203,7 +110,7 @@ for idx in range(len(timetable.df_list)):
                 current_group_list.pop(idx_gr)
                 continue 
 
-            # Finding a Cat 2. element - both neighbors are groups
+            # Finding an unmatched element, that has both neighbors, which are groups
             elif left_is_group and right_is_group:
                 left_group = current_group_list[idx_gr - 1]
                 right_group = current_group_list[idx_gr + 1]
@@ -220,7 +127,8 @@ for idx in range(len(timetable.df_list)):
                 elif dist_to_left > dist_to_right:
                     right_group.insert(0, item)
                     current_group_list.pop(idx_gr)
-                # Join two lists and an element
+                # Joining two lists and an element, 
+                # if the nearest elements from the neighboring groups have the same difference
                 # ex. [..., group0, elem0, group1, ...] -> group0-elem0-group1
                 else:
                     left_group.append(item)
